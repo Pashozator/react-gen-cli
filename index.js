62f2bd83f194aac2ec3b8198ba0f2c673ebb6bda
pashozator <wojciech.szucko@gmail.com>
2019-05-23 18:11:42 +0200

    generate functional component

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { createComponent } from './creators/create-component';
 import { createInterceptor } from './creators/create-interceptor';
 import { createService } from './creators/create-service';
 import { createTranslator } from './creators/create-translator';
+import { createFunctionalComponent } from './creators/create-functional-component';
 
 const program = require('commander')
 	.version(require('./package.json').version);
@@ -12,10 +13,16 @@ const program = require('commander')
 program
 	.command('generate <type> <name>')
 	.alias('g')
+	.option('-f, --functional', 'Make functional component')
 	.option('-s, --style <type>', 'Choose style between .css, .scss, .sass and .less', 'scss')
 	.action((type, name, options) => {
 		switch (type) {
 			case 'component':
+				if (options.functional) {
+					createFunctionalComponent(name, options);
+					break;
+				}
+
 				createComponent(name, options);
 				break;
 			case 'interceptor':
@@ -28,7 +35,6 @@ program
 				createTranslator(name, options);
 				break;
 		}
-
 	});
 
 program.parse(process.argv);
