052cf71d97f33ec45375604034a748a24f9c3c3a
Wojciech Szućko <nexsklrs@gmail.com>
2019-05-25 12:32:48 +0200

    fix paths

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,8 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 
-shell.exec(`node --experimental-modules ./lib/index.mjs ${process.argv[2]} ${process.argv[3]} ${process.argv[4]}`);
+const path = process.mainModule.filename;
+const index = path.indexOf('/bin');
+const root = path.slice(0, index);
+
+shell.exec(`node --experimental-modules ${root}/lib/index.mjs ${process.argv[2]} ${root} ${process.argv[3]} ${process.argv[4]}`);
