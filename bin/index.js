58eb9e7e2c025b132046664e0d0a0db92dd004ab
Wojciech Szućko <nexsklrs@gmail.com>
2019-05-25 11:24:06 +0200

    change to mjs

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 
-shell.exec(`node -r esm ./lib/index.js ${process.argv[2]} ${process.argv[3]} ${process.argv[4]}`);
+shell.exec(`node --experimental-modules ./lib/index.mjs ${process.argv[2]} ${process.argv[3]} ${process.argv[4]}`);
