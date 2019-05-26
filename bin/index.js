79dde46a9fc74aefb4c86cb9a04e92950153ed94
Wojciech Szućko <nexsklrs@gmail.com>
2019-05-26 09:21:50 +0200

    add commands and remove global generate command

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,22 @@
 #! /usr/bin/env node
+'use strict';
+
 var shell = require("shelljs");
 
 const path = process.mainModule.filename;
 const index = path.indexOf('/bin');
 const root = path.slice(0, index);
 
-shell.exec(`node --experimental-modules ${root}/lib/index.mjs ${process.argv[2]} ${root} ${process.argv[3]} ${process.argv[4]}`);
+const args = {
+	command: process.argv[2],
+	name: process.argv[3],
+	style: {
+		command: process.argv[4] != null ? process.argv[4] : '',
+		arg: process.argv[5] != null ? process.argv[5] : ''
+	},
+	functional: {
+		command: process.argv[6] != null ? process.argv[6] : '',
+	}
+};
+
+shell.exec(`node --experimental-modules --no-warnings ${root}/lib/index.mjs ${args.command} ${root} ${args.name} ${args.style.command} ${args.style.arg} ${args.functional.command}`);
